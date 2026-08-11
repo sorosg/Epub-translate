cc8fa130d5f52df2b9301e3db65e0a44e52c3b92
sorosg <sorosgergo@gmail.com>
2026-08-11 21:42:08 +0200

    v1.1.0: Verzió bump 1.0.6 → 1.1.0 (EPUB olvasó funkció)

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # EPUB Fordító Rendszer - Telepítő/Frissítő Script v1.0
-# Verzió: 1.0.6
+# Verzió: 1.1.0
 # Kódnév: "Smart Optimizer"
 # Dátum: 2026-08-11
 # Leírás: 1.0.1 hibajavítások – Docker build gyorsítás, frissítés oldal fix,
@@ -26,7 +26,7 @@ WHITE='\033[1;37m'
 NC='\033[0m'
 
 # Verzió
-VERSION="1.0.6"
+VERSION="1.1.0"
 CODENAME="Smart Optimizer"
 RELEASE_DATE="2026-08-11"
 MIN_VERSION_FOR_UPDATE="1.0.0"
